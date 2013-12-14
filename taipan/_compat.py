02eb0e8eca9af627f8e560a2b4bbea1c7211194b
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2013-12-15 00:26:19 +0100

    Compatibility module

---
+++ taipan/_compat.py
@@ -0,0 +1,21 @@
+"""
+Compatibility shims for different Python versions.
+"""
+try:
+    import json
+except ImportError:
+    try:
+        import simplejson as json
+    except ImportError:
+        import django.utils.simplejson as json
+
+
+import sys
+IS_PY3 = sys.version[0] == '3'
+
+if IS_PY3:
+    imap = map
+    unichr = chr
+    xrange = range
+else:
+    from itertools import imap

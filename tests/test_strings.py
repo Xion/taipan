6512306ac3bd57a93cd718d192f5b2709a123e8c
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2013-12-15 23:00:03 +0100

    More .strings tests

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -34,4 +34,41 @@ class IsString(TestCase):
 
 
 class EnsureString(TestCase):
+    DEFAULT_STRING = 'foo'
+    UNICODE_STRING = u'foo'
+    BYTE_STRING = b'foo'
+
+    def test_none(self):
+        with self.assertRaises(TypeError):
+            __unit__.ensure_string(None)
+
+    def test_some_object(self):
+        with self.assertRaises(TypeError):
+            __unit__.ensure_string(object())
+
+    def test_default_string(self):
+        __unit__.ensure_string(self.DEFAULT_STRING)
+
+    def test_unicode_string(self):
+        __unit__.ensure_string(self.UNICODE_STRING)
+
+    @skipIf(IS_PY3, "requires Python 2.x")
+    def test_bytestring__py2(self):
+        __unit__.ensure_string(self.BYTE_STRING)
+
+    @skipUnless(IS_PY3, "requires Python 3.x")
+    def test_bytestring__py3(self):
+        with self.assertRaises(TypeError):
+            __unit__.ensure_string(self.DEFAULT_STRING)
+
+
+class Join(TestCase):
+    pass
+
+
+class CamelCase(TestCase):
+    pass
+
+
+class Replace(TestCase):
     pass

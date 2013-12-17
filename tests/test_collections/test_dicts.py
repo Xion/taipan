19908cb555c3db4b5f498b58058743928527901e
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2013-12-17 23:12:51 +0100

    Skeleton tests for .collections.dicts

---
+++ tests/test_collections/test_dicts.py
@@ -0,0 +1,34 @@
+"""
+Tests for the .collections.dicts module.
+"""
+from taipan.testing import TestCase
+
+import taipan.collections.dicts as __unit__
+
+
+class FilterItems(TestCase):
+    pass
+
+
+class FilterKeys(TestCase):
+    pass
+
+
+class FilterValues(TestCase):
+    pass
+
+
+class Get(TestCase):
+    pass
+
+
+class Merge(TestCase):
+    pass
+
+
+class Reverse(TestCase):
+    pass
+
+
+class Select(TestCase):
+    pass

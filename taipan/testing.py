2aa16d33045ade5b6267fbc8f68974b440fd0b63
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2013-12-23 17:52:17 +0100

    Skip functions in .testing

--- taipan/testing.py
+++ taipan/testing.py
@@ -11,6 +11,7 @@ try:
 except ImportError:
     from unittest import *
 
+from taipan.functional import identity
 from taipan.strings import BaseString
 
 
@@ -24,6 +25,8 @@ class TestCase(_BaseTestCase):
 
     Includes few additional, convenience assertion methods.
     """
+    __missing = object()
+
     def assertStartsWith(self, prefix, string, msg=None):
         """Assert that ``string`` starts with given ``prefix``."""
         self.assertIsInstance(prefix, BaseString)
@@ -38,7 +41,68 @@ class TestCase(_BaseTestCase):
         if not string.endswith(suffix):
             self.__fail(msg, "%s does not end with %s" % (string, suffix))
 
+    def assertThat(self, predicate, argument=__missing, msg=None):
+        """Assert that a ``predicate`` applies to given ``argument``.
+
+        Example::
+
+            self.assertThat(is_pair, some_tuple)
+        """
+        if not callable(predicate):
+            self.fail("%s is not a callable predicate" % predicate)
+
+        satisfied = (
+            predicate() if argument is self.__missing else predicate(argument))
+        if not satisfied:
+            self.__fail(
+                msg, "predicate %s not satisfied for %s" % (predicate, string))
+
     # Utility functions
 
     def __fail(self, custom_msg, standard_msg):
         self.fail(self._formatMessage(custom_msg, standard_msg))
+
+
+# Skip decorators
+
+# TODO(xion): come up with better name
+def skipIfReturnsTrue(predicate):
+    """Decorator that will cause a test to be skipped
+    if given ``predicate`` callable evaluates to true.
+    """
+    if predicate():
+        desc = getattr(predicate, '__doc__', None) or repr(predicate)
+        return skip("predicate evaluated to true: %s", desc)
+    return identity()
+
+
+def skipUnlessReturnsTrue(predicate):
+    """Decorator that will cause a test to be skipped
+    unless given ``predicate`` callable evaluates to true.
+    """
+    if not predicate():
+        desc = getattr(predicate, '__doc__', None) or repr(predicate)
+        return skip("predicate evaluated to false: %s", desc)
+    return identity()
+
+# TODO(xion): seriously weigh pros & cons of having those
+skipIfReturnsFalse = skipUnlessReturnsTrue
+skipUnlessReturnsFalse = skipIfReturnsTrue
+
+
+def skipIfHasattr(obj, attr):
+    """Decorator that will cause a test to be skipped
+    if given ``object`` contains given ``attr``\ ibute.
+    """
+    if hasattr(obj, attr):
+        return skip("%r has attribute %r" % (obj, attr))
+    return identity()
+
+
+def skipUnlessHasattr(obj, attr):
+    """Decorator that will cause a test to be skipped
+    unless given ``object`` contains given ``attr``\ ibute.
+    """
+    if not hasattr(obj, attr):
+        return skip("%r does not have attribute %r" % (obj, attr))
+    return identity()

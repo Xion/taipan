acfa03483b4f6e4fdced9d6aef83bea2415b66b3
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2013-12-25 17:52:37 +0100

    Refactor test_testing.py slightly

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -26,49 +26,51 @@ class _Assertion(TestCase):
             pass
 
     _TESTCASE = _DummyTestCase()
-    _FAILURE = _TESTCASE.failureException
+
+    def _assertFailure(self):
+        return self.assertRaises(self._TESTCASE.failureException)
 
 
 class AssertZero(_Assertion):
 
     def test_none(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertZero(None)
 
     def test_empty_string(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertZero("")
 
     def test_empty_list(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertZero([])
 
     def test_empty_tuple(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertZero(())
 
     def test_empty_dict(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertZero({})
 
     def test_some_object(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertZero(object())
 
     def test_positive_integer(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertZero(42)
 
     def test_negative_integer(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertZero(-69)
 
     def test_positive_float(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertZero(3.14)
 
     def test_negative_float(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertZero(-2.73)
 
     def test_integer_zero(self):
@@ -83,26 +85,26 @@ class AssertStartsWith(_Assertion):
     STRING = "foobar"
 
     def test_prefix__none(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertStartsWith(None, self.STRING)
 
     def test_prefix__some_object(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertStartsWith(object(), self.STRING)
 
     def test_prefix__empty_string(self):
         self._TESTCASE.assertStartsWith("", self.STRING)
 
     def test_string__none(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertStartsWith(self.PREFIX, None)
 
     def test_string__some_object(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertStartsWith(self.PREFIX, object())
 
     def test_string__empty_string(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertStartsWith(self.PREFIX, "")
 
     def test_success(self):
@@ -114,26 +116,26 @@ class AssertEndsWith(_Assertion):
     STRING = "foobar"
 
     def test_suffix__none(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertEndsWith(None, self.STRING)
 
     def test_suffix__some_object(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertEndsWith(object(), self.STRING)
 
     def test_suffix__empty_string(self):
         self._TESTCASE.assertEndsWith("", self.STRING)
 
     def test_string__none(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertEndsWith(self.SUFFIX, None)
 
     def test_string__some_object(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertEndsWith(self.SUFFIX, object())
 
     def test_string__empty_string(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertEndsWith(self.SUFFIX, "")
 
     def test_success(self):
@@ -145,18 +147,18 @@ class AssertThat(_Assertion):
     NOT = staticmethod(lambda x: not x)
 
     def test_predicate__none(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertThat(None)
 
     def test_predicate__non_callable(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertThat(object())
 
     def test_predicate__truth(self):
         self._TESTCASE.assertThat(lambda: True)
 
     def test_predicate__falsity(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertThat(lambda: False)
 
     def test_predicate__expected_argument(self):
@@ -164,13 +166,13 @@ class AssertThat(_Assertion):
             self._TESTCASE.assertThat(self.IDENTITY)
 
     def test_argument__none(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertThat(AssertThat.IDENTITY, None)
         self._TESTCASE.assertThat(AssertThat.NOT, None)
 
     def test_argument__some_object(self):
         self._TESTCASE.assertThat(AssertThat.IDENTITY, object())
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertThat(AssertThat.NOT, object())
 
 
@@ -181,16 +183,16 @@ class AssertNoop(_Assertion):
     ARGUMENT = 42
 
     def test_function__none(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertNoop(None, self.ARGUMENT)
 
     def test_function__non_callable(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertNoop(object(), self.ARGUMENT)
 
     def test_function__noop(self):
         self._TESTCASE.assertNoop(self.NOOP, self.ARGUMENT)
 
     def test__function__effective(self):
-        with self.assertRaises(self._FAILURE):
+        with self._assertFailure():
             self._TESTCASE.assertNoop(self.EFFECTIVE, self.ARGUMENT)

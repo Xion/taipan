6bb60ad1437fb2cc43bedfad56465192ee8a5bdf
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2014-01-01 13:07:21 +0100

    Refactor .api.decorators tests

--- tests/test_api/test_decorators.py
+++ tests/test_api/test_decorators.py
@@ -9,8 +9,64 @@ from taipan.testing import TestCase
 import taipan.api.decorators as __unit__
 
 
-class _FunctionDecorator(TestCase):
+class _Decorator(TestCase):
+    """Base class for @*decorator test cases."""
     DECORATOR_ARG = 42
+
+    def setUp(self):
+        self.fail("setUp() must be overriden in derived testcases")
+
+    def _assertIsDecorated(self, obj, arg=None):
+        self.assertHasAttr('__decorated__', obj)
+        self.assertTrue(obj.__decorated__)
+        if arg is not None:
+            self.assertHasAttr('__decorator_arg__', obj)
+            self.assertEquals(arg, obj.__decorator_arg__)
+
+    def _create_argless_decorator_function(self):
+        @self.decorator_under_test
+        def decorator():
+            def wrapper(f):
+                f.__decorated__ = True
+                return f
+            return wrapper
+        return decorator
+
+    def _create_decorator_function_with_args(self):
+        @self.decorator_under_test
+        def decorator(arg):
+            def wrapper(f):
+                f.__decorated__ = True
+                f.__decorator_arg__ = arg
+                return f
+            return wrapper
+        return decorator
+
+    def _create_argless_decorator_class(self):
+        @self.decorator_under_test
+        class Decorator(object):
+            def __call__(self, f):
+                f.__decorated__ = True
+                return f
+        return Decorator
+
+    def _create_decorator_class_with_args(self):
+        @self.decorator_under_test
+        class Decorator(object):
+            def __init__(self, arg):
+                self.arg = arg
+            def __call__(self, f):
+                f.__decorated__ = True
+                f.__decorator_arg__ = self.arg
+                return f
+        return Decorator
+
+
+class _FunctionDecorator(_Decorator):
+    """Tests for @function_decorator."""
+
+    def setUp(self):
+        self.decorator_under_test = __unit__.function_decorator
 
     def test_none(self):
         with self.assertRaises(TypeError):
@@ -28,8 +84,8 @@ class _FunctionDecorator(TestCase):
         @decorator()
         def bar():
             pass
-        self.assertTrue(foo.__decorated__)
-        self.assertTrue(bar.__decorated__)
+        self._assertIsDecorated(foo)
+        self._assertIsDecorated(bar)
 
     def test_decorator_function__no_args__apply_to_class(self):
         decorator = self._create_argless_decorator_function()
@@ -47,8 +103,7 @@ class _FunctionDecorator(TestCase):
         @decorator(self.DECORATOR_ARG)
         def foo():
             pass
-        self.assertTrue(foo.__decorated__)
-        self.assertEquals(self.DECORATOR_ARG, foo.__decorator_arg__)
+        self._assertIsDecorated(foo, self.DECORATOR_ARG)
 
     def test_decorator_function__with_args__apply_to_function__badly(self):
         decorator = self._create_decorator_function_with_args()
@@ -76,8 +131,8 @@ class _FunctionDecorator(TestCase):
         @decorator()
         def bar():
             pass
-        self.assertTrue(foo.__decorated__)
-        self.assertTrue(bar.__decorated__)
+        self._assertIsDecorated(foo)
+        self._assertIsDecorated(bar)
 
     def test_deorator_class__no_args__apply_to_class(self):
         decorator = self._create_argless_decorator_class()
@@ -95,8 +150,7 @@ class _FunctionDecorator(TestCase):
         @decorator(self.DECORATOR_ARG)
         def foo():
             pass
-        self.assertTrue(foo.__decorated__)
-        self.assertEquals(self.DECORATOR_ARG, foo.__decorator_arg__)
+        self._assertIsDecorated(foo, self.DECORATOR_ARG)
 
     def test_decorator_class__with_args__apply_to_function__badly(self):
         decorator = self._create_decorator_class_with_args()
@@ -115,49 +169,3 @@ class _FunctionDecorator(TestCase):
             @decorator(self.DECORATOR_ARG)
             class Foo(object):
                 pass
-
-    # Utility functions
-
-    def _create_argless_decorator_function(self):
-        @__unit__.function_decorator
-        def decorator():
-            def wrapper(f):
-                self.assertTrue(inspect.isfunction(f))
-                f.__decorated__ = True
-                return f
-            return wrapper
-        return decorator
-
-    def _create_decorator_function_with_args(self):
-        @__unit__.function_decorator
-        def decorator(arg):
-            def wrapper(f):
-                self.assertTrue(inspect.isfunction(f))
-                f.__decorated__ = True
-                f.__decorator_arg__ = arg
-                return f
-            return wrapper
-        return decorator
-
-    def _create_argless_decorator_class(self):
-        testcase = self  # because shadowing
-        @__unit__.function_decorator
-        class Decorator(object):
-            def __call__(self, f):
-                testcase.assertTrue(inspect.isfunction(f))
-                f.__decorated__ = True
-                return f
-        return Decorator
-
-    def _create_decorator_class_with_args(self):
-        testcase = self  # because shadowing
-        @__unit__.function_decorator
-        class Decorator(object):
-            def __init__(self, arg):
-                self.arg = arg
-            def __call__(self, f):
-                testcase.assertTrue(inspect.isfunction(f))
-                f.__decorated__ = True
-                f.__decorator_arg__ = self.arg
-                return f
-        return Decorator

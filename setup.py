f86f2f84601d2987f725bd1484dad0cf1f650594
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2014-01-05 22:03:04 +0100

    Smarter (and longer) setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,50 @@ taipan
 General purpose toolkit for Python
 """
 from setuptools import find_packages, setup
+import os
+import sys
 
 import taipan
+
+
+def read_requirements(filename='requirements.txt'):
+    """Reads the list of requirements from given file.
+
+    :param filename: Filename to read the requirements from.
+                     Uses ``'requirements.txt'`` by default.
+
+    :return: Requirments as list of strings.
+    """
+    # allow for some leeway with the argument
+    if not filename.startswith('requirements'):
+        filename = 'requirements-' + filename
+    if not os.path.splitext(filename)[1]:
+        filename += '.txt'  # no extension, add default
+
+    def valid_line(line):
+        line = line.strip()
+        return line and not any(line.startswith(p) for p in ('#', '-'))
+
+    def extract_requirement(line):
+        egg_eq = '#egg='
+        if egg_eq in line:
+            _, requirement = line.split(egg_eq, 1)
+            return requirement
+        return line
+
+    with open(filename) as f:
+        lines = f.readlines()
+        return list(map(extract_requirement, filter(valid_line, lines)))
+
+
+def get_test_requirements():
+    """Get the list of test requirements
+    for ``tests_require`` parameter of ``setup()``.
+    """
+    requirements = read_requirements('test')
+    if sys.version_info <= (2, 7):
+        requirements.extend(read_requirements('test-py26'))
+    return requirements
 
 
 setup(
@@ -36,5 +78,5 @@ setup(
     platforms='any',
     packages=find_packages(exclude=['tests']),
 
-    tests_require=open('requirements-test.txt').readlines(),
+    tests_require=get_test_requirements(),
 )

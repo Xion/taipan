8284b6992b741b9fd12dd22e3d02990827c6c8b1
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2014-01-18 23:50:28 +0100

    Add k_subsets & trivial_partition to .collections.sets

--- taipan/collections/sets.py
+++ taipan/collections/sets.py
@@ -3,9 +3,30 @@ Set-related functions and classes.
 """
 from itertools import chain, combinations
 
-from taipan._compat import imap, xrange
+from taipan._compat import imap, xrange, Numeric
 from taipan.collections import ensure_countable, is_set
 from taipan.collections.tuples import is_tuple
+
+
+__all__ = ['k_subsets', 'power', 'powerset', 'trivial_partition']
+
+
+def k_subsets(set_, k):
+    """Return subsets of given set with given cardinality.
+    :param k: Cardinality of subsets to return
+    :return: Iterable containing all ``k``-subsets of given set
+    """
+    ensure_countable(set_)
+
+    if not isinstance(k, Numeric):
+        raise TypeError("subset cardinality must be a number")
+    if not (k >= 0):
+        raise ValueError("subset cardinality must be positive")
+    if not (k <= len(set_)):
+        raise ValueError("subset cardinality must not exceed set cardinality")
+
+    result = combinations(set_, k)
+    return _harmonize_subset_types(set_, result)
 
 
 def power(set_):
@@ -18,11 +39,35 @@ def power(set_):
 
     result = chain.from_iterable(combinations(set_, r)
                                  for r in xrange(len(set_) + 1))
+    return _harmonize_subset_types(set_, result)
 
+#: Alias for :func:`power`.
+powerset = power
+
+
+def trivial_partition(set_):
+    """Returns a parition of given set into 1-element subsets.
+
+    :return: Trivial partition of given set, i.e. iterable containing disjoint
+             1-element sets, each consisting of a single element
+             from given set
+    """
+    ensure_countable(set_)
+
+    result = ((x,) for x in set_)
+    return _harmonize_subset_types(set_, result)
+
+
+# Utility functions
+
+def _harmonize_subset_types(set_, subset_tuples):
+    """Possibly convert an iterable of tuples with subsets of given "set",
+    to an iterable of :class:`set` objects if original "set" was so too.
+    """
     # if argument is originally a set, return subsets also as sets;
     # otherwise (for non-set collection), return subsets as tuples
     if is_tuple(set_):
-        return result  # combinations() returns tuples; need not convert them
+        return subset_tuples
     else:
         subset_class = set_.__class__ if is_set(set_) else tuple
-        return imap(subset_class, result)
+        return imap(subset_class, subset_tuples)
--- tests/test_collections/test_sets.py
+++ tests/test_collections/test_sets.py
@@ -9,11 +9,64 @@ from taipan.testing import TestCase
 import taipan.collections.sets as __unit__
 
 
-class Power(TestCase):
+class _SubsetOperation(TestCase):
     TWO_ELEMS_TUPLE = (1, 2)
-    POWERSET_OF_TWO_ELEMS_TUPLE = [(), (1,), (2,), (1, 2)]
-
     TWO_ELEMS_SET = set([1, 2])
+
+
+class KSubsets(_SubsetOperation):
+
+    def test_set__none(self):
+        with self.assertRaises(TypeError):
+            __unit__.k_subsets(None, 1)
+
+    def test_set__some_object(self):
+        with self.assertRaises(TypeError):
+            __unit__.k_subsets(object(), 1)
+
+    def test_set__empty_iterable(self):
+        self.assertItemsEqual([()], __unit__.k_subsets([], 0))
+
+    def test_set__singleton_iterable(self):
+        self.assertItemsEqual([(42,)], __unit__.k_subsets([42], 1))
+
+    def test_set__singleton_set(self):
+        set_ = set([42])
+        self.assertItemsEqual([set_], __unit__.k_subsets(set_, 1))
+
+    def test_k__none(self):
+        with self.assertRaises(TypeError):
+            __unit__.k_subsets(self.TWO_ELEMS_SET, None)
+
+    def test_k__some_object(self):
+        with self.assertRaises(TypeError):
+            __unit__.k_subsets(self.TWO_ELEMS_SET, object())
+
+    def test_k__negative(self):
+        with self.assertRaises(ValueError):
+            __unit__.k_subsets(self.TWO_ELEMS_SET, -1)
+
+    def test_k__zero(self):
+        self.assertItemsEqual([set()], __unit__.k_subsets(set(), 0))
+        self.assertItemsEqual(
+            [set()], __unit__.k_subsets(self.TWO_ELEMS_SET, 0))
+
+    def test_k__one(self):
+        self.assertItemsEqual(
+            [set((x,)) for x in self.TWO_ELEMS_SET],
+            __unit__.k_subsets(self.TWO_ELEMS_SET, 1))
+
+    def test_k__positive(self):
+        self.assertItemsEqual(
+            [self.TWO_ELEMS_SET], __unit__.k_subsets(self.TWO_ELEMS_SET, 2))
+
+    def test_k__too_big(self):
+        with self.assertRaises(ValueError):
+            __unit__.k_subsets(self.TWO_ELEMS_SET, len(self.TWO_ELEMS_SET) + 1)
+
+
+class Power(_SubsetOperation):
+    POWERSET_OF_TWO_ELEMS_TUPLE = [(), (1,), (2,), (1, 2)]
     POWERSET_OF_TWO_ELEMS_SET = [set(), set([1]), set([2]), set([1, 2])]
 
     EIGHT_ELEMS_TUPLE = tuple(xrange(8))
@@ -58,3 +111,39 @@ class Power(TestCase):
         powerset = list(__unit__.power(self.EIGHT_ELEMS_SET))
         self.assertAll(is_set, powerset)
         self.assertEquals(2 ** len(self.EIGHT_ELEMS_SET), len(powerset))
+
+
+class TrivialPartition(_SubsetOperation):
+    TRIVIAL_PARTITION_OF_TWO_ELEMS_TUPLE = [(1,), (2,)]
+    TRIVIAL_PARTITION_OF_TWO_ELEMS_SET = [set((1,)), set((2,))]
+
+    def test_none(self):
+        with self.assertRaises(TypeError):
+            __unit__.trivial_partition(None)
+
+    def test_some_object(self):
+        with self.assertRaises(TypeError):
+            __unit__.trivial_partition(object())
+
+    def test_empty_iterable(self):
+        self.assertEmpty(__unit__.trivial_partition([]))
+
+    def test_singleton_iterable(self):
+        self.assertItemsEqual([(42,)], __unit__.trivial_partition([42]))
+
+    def test_empty_set(self):
+        self.assertEmpty(__unit__.trivial_partition(set()))
+
+    def test_singleton_set(self):
+        set_ = set([42])
+        self.assertItemsEqual([set_], __unit__.trivial_partition(set_))
+
+    def test_cardinality2_iterable(self):
+        self.assertItemsEqual(
+            self.TRIVIAL_PARTITION_OF_TWO_ELEMS_TUPLE,
+            __unit__.trivial_partition(self.TWO_ELEMS_TUPLE))
+
+    def test_cardinality2_set(self):
+        self.assertItemsEqual(
+            self.TRIVIAL_PARTITION_OF_TWO_ELEMS_SET,
+            __unit__.trivial_partition(self.TWO_ELEMS_SET))

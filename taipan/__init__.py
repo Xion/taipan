a6a657788255570313bac52c8f09903a605165fb
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2014-08-30 12:14:54 -0700

    Update some top-level files

--- taipan/__init__.py
+++ taipan/__init__.py
@@ -5,3 +5,7 @@ __version__ = "0.0.3"
 __description__ = "General purpose toolkit for Python"
 __author__ = "Karol Kuczmarski"
 __license__ = "Simplified BSD"
+
+
+# TODO(xion): add the pkg_resources shim, allowing the package to be split
+# between multiple cheeseshop distributions

a4c354d5a05929070b7259f97bb6c8433c12f057
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2014-10-09 21:29:03 -0700

    Move the @metaclass decorator to ._compat

--- taipan/_compat.py
+++ taipan/_compat.py
@@ -1,6 +1,13 @@
 """
 Compatibility shims for different Python versions and platforms.
 """
+import sys
+IS_PY26 = sys.version_info[:2] == (2, 6)
+IS_PY3 = sys.version_info[0] == 3
+
+
+# Modules
+
 try:
     import builtins
 except ImportError:
@@ -15,10 +22,7 @@ except ImportError:
         import django.utils.simplejson as json
 
 
-import sys
-IS_PY26 = sys.version_info[:2] == (2, 6)
-IS_PY3 = sys.version_info[0] == 3
-
+# Global symbols
 
 unichr = chr if IS_PY3 else unichr
 xrange = range if IS_PY3 else xrange
@@ -33,3 +37,49 @@ if IS_PY3:
     )
 else:
     from itertools import ifilter, ifilterfalse, imap, izip, izip_longest
+
+
+# Other
+
+class MetaclassDecorator(object):
+    """Decorator for creating a class through a metaclass.
+
+    Unlike ``__metaclass__`` attribute from Python 2, or ``metaclass=`` keyword
+    argument from Python 3, the ``@metaclass`` decorator works with both
+    versions of the language.
+
+    Example::
+
+        @metaclass(MyMetaclass)
+        class MyClass(object):
+            pass
+    """
+    def __init__(self, meta):
+        if not issubclass(meta, type):
+            raise TypeError(
+                "expected a metaclass, got %s instead" % type(meta).__name__)
+        self.metaclass = meta
+
+    def __call__(self, cls):
+        """Apply the decorator to given class.
+
+        This recreates the class using the previously supplied metaclass.
+        """
+        # Copyright (c) Django Software Foundation and individual contributors.
+        # All rights reserved.
+
+        original_dict = cls.__dict__.copy()
+        original_dict.pop('__dict__', None)
+        original_dict.pop('__weakref__', None)
+
+        slots = original_dict.get('__slots__')
+        if slots is not None:
+            if isinstance(slots, str):
+                slots = [slots]
+            for slot in slots:
+                original_dict.pop(slot)
+
+        return self.metaclass(cls.__name__, cls.__bases__, original_dict)
+
+metaclass = MetaclassDecorator
+del MetaclassDecorator
--- taipan/objective/classes.py
+++ taipan/objective/classes.py
@@ -3,6 +3,7 @@ Class related functions and utilities.
 """
 import inspect
 
+from taipan._compat import metaclass
 from taipan.algorithms import breadth_first
 from taipan.api.decorators import _wrap_decorator
 from taipan.functional.combinators import and_, not_
@@ -101,45 +102,7 @@ def ensure_metaclass(arg):
     return arg
 
 
-class MetaclassDecorator(object):
-    """Decorator for creating a class through a metaclass.
-
-    Unlike ``__metaclass__`` attribute from Python 2, or ``metaclass=`` keyword
-    argument from Python 3, the ``@metaclass`` decorator works with both
-    versions of the language.
-
-    Example::
-
-        @metaclass(MyMetaclass)
-        class MyClass(object):
-            pass
-    """
-    def __init__(self, meta):
-        self.metaclass = ensure_metaclass(meta)
-
-    def __call__(self, cls):
-        """Apply the decorator to given class.
-
-        This recreates the class using the previously supplied metaclass.
-        """
-        # Copyright (c) Django Software Foundation and individual contributors.
-        # All rights reserved.
-
-        original_dict = cls.__dict__.copy()
-        original_dict.pop('__dict__', None)
-        original_dict.pop('__weakref__', None)
-
-        slots = original_dict.get('__slots__')
-        if slots is not None:
-            if isinstance(slots, str):
-                slots = [slots]
-            for slot in slots:
-                original_dict.pop(slot)
-
-        return self.metaclass(cls.__name__, cls.__bases__, original_dict)
-
-# The decorator can be applied to classes that aren't themselves metaclasses.
-metaclass = _wrap_decorator(MetaclassDecorator, "non-meta classes",
+# Expose the :class:`metaclass` decorator after augmenting it with type checks.
+metaclass = _wrap_decorator(metaclass, "non-meta classes",
                             and_(inspect.isclass, not_(is_metaclass)))
 metaclass.__name__ = 'metaclass'
-del MetaclassDecorator

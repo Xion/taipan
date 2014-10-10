437778bcd5cc6dc9ab0838e214bface89e4c6f60
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2014-10-09 22:04:45 -0700

    Bump version number to 0.0.4

--- taipan/__init__.py
+++ taipan/__init__.py
@@ -1,7 +1,7 @@
 """
 taipan
 """
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __description__ = "General purpose toolkit for Python"
 __author__ = "Karol Kuczmarski"
 __license__ = "Simplified BSD"

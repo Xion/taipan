2ff7953c8e362afff15067c7c7f7fd1c2e3c35c8
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-03-21 17:35:51 -0700

    Tests for .collections.lists.find/findlast

--- tests/test_collections/test_lists.py
+++ tests/test_collections/test_lists.py
@@ -1,6 +1,8 @@
 """
 Tests for the .collections.lists module.
 """
+from contextlib import contextmanager
+
 from taipan.testing import TestCase
 
 import taipan.collections.lists as __unit__
@@ -98,7 +100,7 @@ class Init(TestCase):
 
 # Searching for elements
 
-class _Index(TestCase):
+class _Search(TestCase):
     LIST = [0, 1, 2, 1, 0]
 
     EVEN = staticmethod(lambda x: x % 2 == 0)
@@ -106,7 +108,116 @@ class _Index(TestCase):
     NEGATIVE = staticmethod(lambda x: x < 0)
 
 
-class Index(_Index):
+class _Find(_Search):
+
+    @contextmanager
+    def assertNotFound(self):
+        with self.assertRaises(IndexError) as r:
+            yield r
+        self.assertIn("not found", str(r.exception))
+
+
+class Find(_Find):
+    FIRST_EVEN = 0
+    FIRST_ODD = 1
+
+    def test_positional__list__none(self):
+        with self.assertRaises(TypeError):
+            __unit__.find(None, None)
+
+    def test_positional__list__some_object(self):
+        with self.assertRaises(TypeError):
+            __unit__.find(None, object())
+
+    def test_positional__list__empty(self):
+        with self.assertNotFound():
+            __unit__.find(None, [])
+        for elem in self.LIST:
+            with self.assertNotFound():
+                __unit__.find(elem, [])
+
+    def test_positional__list__normal(self):
+        for elem in self.LIST:
+            self.assertEquals(elem, __unit__.find(elem, self.LIST))
+
+    def test_keyword__only_list(self):
+        with self.assertRaises(TypeError):
+            __unit__.find(in_=self.LIST)
+
+    def test_keyword__of__absent(self):
+        with self.assertNotFound():
+            __unit__.find(of=None, in_=self.LIST)
+
+    def test_keyword__of__present(self):
+        for elem in self.LIST:
+            self.assertEquals(elem, __unit__.find(of=elem, in_=self.LIST))
+
+    def test_keyword__where__absent(self):
+        with self.assertNotFound():
+            __unit__.find(where=self.NEGATIVE, in_=self.LIST)
+
+    def test_keyword__where__present(self):
+        self.assertEquals(
+            self.FIRST_EVEN, __unit__.find(where=self.EVEN, in_=self.LIST))
+        self.assertEquals(
+            self.FIRST_ODD, __unit__.find(where=self.ODD, in_=self.LIST))
+
+    def test_keyword__both(self):
+        with self.assertRaises(TypeError):
+            __unit__.find(of=None, where=self.EVEN, in_=self.LIST)
+
+
+class FindLast(_Find):
+    LAST_EVEN = 0
+    LAST_ODD = 1
+
+    def test_positional__list__none(self):
+        with self.assertRaises(TypeError):
+            __unit__.findlast(None, None)
+
+    def test_positional__list__some_object(self):
+        with self.assertRaises(TypeError):
+            __unit__.findlast(None, object())
+
+    def test_positional__list__empty(self):
+        with self.assertNotFound():
+            __unit__.findlast(None, [])
+        for elem in self.LIST:
+            with self.assertNotFound():
+                __unit__.findlast(elem, [])
+
+    def test_positional__list__normal(self):
+        for elem in self.LIST:
+            self.assertEquals(elem, __unit__.findlast(elem, self.LIST))
+
+    def test_keyword__only_list(self):
+        with self.assertRaises(TypeError):
+            __unit__.findlast(in_=self.LIST)
+
+    def test_keyword__of__absent(self):
+        with self.assertNotFound():
+            __unit__.findlast(of=None, in_=self.LIST)
+
+    def test_keyword__of__present(self):
+        for elem in self.LIST:
+            self.assertEquals(elem, __unit__.findlast(of=elem, in_=self.LIST))
+
+    def test_keyword__where__absent(self):
+        with self.assertNotFound():
+            __unit__.findlast(where=self.NEGATIVE, in_=self.LIST)
+
+    def test_keyword__where__present(self):
+        self.assertEquals(
+            self.LAST_EVEN, __unit__.findlast(where=self.EVEN, in_=self.LIST))
+        self.assertEquals(
+            self.LAST_ODD, __unit__.findlast(where=self.ODD, in_=self.LIST))
+
+    def test_keyword__both(self):
+        with self.assertRaises(TypeError):
+            __unit__.findlast(of=None, where=self.EVEN, in_=self.LIST)
+
+
+class Index(_Search):
     INDICES = [0, 1, 2]
 
     EVEN_INDEX = 0
@@ -157,7 +268,7 @@ class Index(_Index):
             __unit__.index(of=None, where=self.EVEN, in_=self.LIST)
 
 
-class LastIndex(_Index):
+class LastIndex(_Search):
     LAST_INDICES = [4, 3, 2]
 
     EVEN_LAST_INDEX = 4
